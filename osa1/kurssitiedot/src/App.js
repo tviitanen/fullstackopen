a5d089a37e1b058e8cc1130bb96a9c2b31ad3944
Teemu Viitanen <92275334+tviitanen@users.noreply.github.com>
2022-05-08 18:59:04 +0300

    1.2

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -11,19 +11,44 @@ const Content = (props) => {
 
   return (
     <div>
+      <Part1 part1={props.part1} exercises1={props.exercises1}/>
+      <Part2 part2={props.part2} exercises2={props.exercises2}/>
+      <Part3 part3={props.part3} exercises3={props.exercises3}/>
+      </div>
+      )
+  }
+const Part1 = (props) => {
+
+  return (
+    <div>
       <p>
         {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
       </p>
       </div>
       )
   }
 
+const Part2 = (props) => {
+
+  return (
+    <div>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      </div>
+      )
+  }
+
+const Part3 = (props) => {
+
+  return (
+    <div>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+      </div>
+      )
+  }  
 const Total = (props) => {
 
   return (
