7bedbc0fb54c9f16831e87a381febfad326b41f4
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-05-24 21:16:21 +0300

    1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,48 +1,85 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Header = () => <div><h1>give feedback</h1></div>
+const Header = () => (
+  <div>
+    <h1>give feedback</h1>
+  </div>
+);
 
-const Statistics = () => <div><h1>statistics</h1></div>
+const Statistics = () => (
+  <div>
+    <h1>statistics</h1>
+  </div>
+);
 
 const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-const DisplayGood = props => <div>good {props.value}</div>
-const DisplayNeutral = props => <div>neutral {props.value}</div>
-const DisplayBad = props => <div>bad {props.value}</div>
-
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const setToGood = newValue => {
-    console.log('value now:', newValue)
-    setGood(newValue)
-  }
-  const setToNeutral = newValue => {
-    console.log('value now:', newValue)
-    setNeutral(newValue)
-  }
-  const setToBad = newValue => {
-    console.log('value now:', newValue)
-    setBad(newValue)
-  }    
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+const DisplayGood = (props) => <div>good {props.value}</div>;
+const DisplayNeutral = (props) => <div>neutral {props.value}</div>;
+const DisplayBad = (props) => <div>bad {props.value}</div>;
+const DisplayAll = (props) => {
   return (
     <div>
-      <Header/>
-      <Button handleClick={() => setToGood(good+1)} text="good" />
-      <Button handleClick={() => setToNeutral(neutral+1)} text="neutral" />
-      <Button handleClick={() => setToBad(bad+1)} text="bad" />
-      <Statistics/>
-      <DisplayGood value={good} />
-      <DisplayNeutral value={bad} />
-      <DisplayBad value={neutral} />
+      all {props.valueArray[0] + props.valueArray[1] + props.valueArray[2]}{" "}
     </div>
-  )
-}
+  );
+};
+const DisplayAverage = (props) => {
+  return (
+    <div>
+      average{" "}
+      {(props.valueArray[0] * 1 +
+        props.valueArray[1] * 0 +
+        props.valueArray[2] * -1) /
+        (props.valueArray[0] + props.valueArray[1] + props.valueArray[2])}
+    </div>
+  );
+};
+const DisplayPositive = (props) => {
+  return (
+    <div>
+      positive{" "}
+      {(props.valueArray[0] /
+        (props.valueArray[0] + props.valueArray[1] + props.valueArray[2])) *
+        100}{" "}
+      %
+    </div>
+  );
+};
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export default App
+  const setToGood = (newValue) => {
+    console.log("value now:", newValue);
+    setGood(newValue);
+  };
+  const setToNeutral = (newValue) => {
+    console.log("value now:", newValue);
+    setNeutral(newValue);
+  };
+  const setToBad = (newValue) => {
+    console.log("value now:", newValue);
+    setBad(newValue);
+  };
+  return (
+    <div>
+      <Header />
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <Statistics />
+      <DisplayGood value={good} />
+      <DisplayNeutral value={neutral} />
+      <DisplayBad value={bad} />
+      <DisplayAll valueArray={[good, neutral, bad]} />
+      <DisplayAverage valueArray={[good, neutral, bad]} />
+      <DisplayPositive valueArray={[good, neutral, bad]} />
+    </div>
+  );
+};
+
+export default App;
