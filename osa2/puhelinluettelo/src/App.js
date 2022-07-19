b3cf3da0c4c61f89552bc3fd4c8963f8c7be18d0
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-07-19 05:14:00 +0300

    2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,22 +1,38 @@
 import { useState } from "react";
+import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
-        </div>
-        <div>
+          <input value={newName} onChange={handleNameChange} />
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <Person key={person.id} person={person} />
+      ))}
     </div>
   );
 };
