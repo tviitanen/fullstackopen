05111047f6ca72759a449a868bb130e5be82855f
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-07-20 05:14:59 +0300

    2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -4,22 +4,30 @@ import Person from "./components/Person";
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    // console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
 
     const alreadyExists = persons.some((person) => person.name === newName);
     if (!alreadyExists) {
       setPersons(persons.concat(newPerson));
       setNewName("");
+      setNewNumber("");
     } else {
       window.alert(`${newName} is already added to phonebook`);
     }
@@ -31,6 +39,11 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
